72275193f9dea26933d4aaa0027a24419fd0233d
Arox-Nemesis <135242263+Arox-Nemesis@users.noreply.github.com>
2025-03-21 08:50:10 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -1,10 +1,9 @@
 #(©)Codexbotz
 #rymme
-
 from aiohttp import web
-
-routes = web.RouteTableDef()
-
-@routes.get("/", allow_head=True)
-async def root_route_handler(request):
-    return web.json_response("CodeXBotz")
+ 
+ routes = web.RouteTableDef()
+ 
+ @routes.get("/", allow_head=True)
+ async def root_route_handler(request):
+     return web.json_response("CodeXBotz")
