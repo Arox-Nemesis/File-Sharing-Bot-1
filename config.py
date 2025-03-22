259c577723eeec8864900d4261fe15e6a7bc4030
Arox-Nemesis <135242263+Arox-Nemesis@users.noreply.github.com>
2025-03-22 07:15:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001869167334"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "5862168163"))
 
 #Port
-PORT = os.environ.get("PORT", "8000")
+PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "")
